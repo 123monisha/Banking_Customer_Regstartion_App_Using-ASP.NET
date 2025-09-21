24b721d958abf2e4d4a521ea26a54750fe12f296
Monisha Ishwar <monishamoger15@gmail.com>
2025-09-21 15:13:41 +0530

    Add project files.

---
+++ Registartion.aspx
@@ -0,0 +1,91 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Registartion.aspx.cs" Inherits="Banking_Customer_Regstartion_App.Registartion" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="bootstrap.css" rel="stylesheet" />
+     <script>
+         function validate() {
+             let name = document.getElementById("txtName").value;
+             let address = document.getElementById("txtAdd").value;
+             let accountSelected = document.getElementById("ddlAccounts").SelectedIndex;
+
+             if (name.trim().length ==0) {
+                 document.getElementById("lblMsg").innerHTML = "Enter Your Name Please";
+                 document.getElementById("lblMsg").style = "color:red";
+                 return false
+             }
+             else if (address.trim().length == 0) {
+                 document.getElementById("lblMsg").innerHTML = "Enter Your Address Please";
+                 document.getElementById("lblMsg").style = "color:red";
+                 return false;
+             }
+             else if (accountSelected==0) {
+                 document.getElementById("lblMsg").innerHTML = "Please Choose Your Account Type";
+                 document.getElementById("lblMsg").style = "color:red";
+                 return false;
+             }
+         }
+     </script>
+</head>
+
+<body>
+    <form id="form1" runat="server">
+       <center> <div>
+            <div style="width:50%; border:solid thin black; padding:20px;">
+                <center>
+                    <h1>Customer  Managemnet</h1>
+                </center>
+            </div>
+             <hr />
+
+ <div>
+     <div style="width:40%; border:solid thin black; padding:20px;  display:inline-block" >
+         <div >
+             Account No: 
+             <asp:Label ID="lblAccNum" runat="server" Text="Label"></asp:Label>
+              
+         </div><br />
+         <div>Name : 
+             <asp:TextBox ID="txtName" runat="server" CssClass="form-control" ></asp:TextBox>
+              
+         </div><br />
+
+         <div>Address : 
+             <asp:TextBox ID="txtAdd" runat="server" CssClass="form-control"></asp:TextBox>
+              
+         </div><br />
+         <div>Preferred Account : 
+             <asp:DropDownList ID="ddlAccounts" runat="server" CssClass="form-control" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged" Width="264px"></asp:DropDownList>
+                    
+                    <br />
+         </div><br />
+
+         <div>Activate Account : 
+             <asp:CheckBox ID="cbStatus" runat="server" CssClass="form-control" OnCheckedChanged="CheckBox1_CheckedChanged" />
+             <br />
+         </div><br />
+         <div>
+
+         <div>
+             <asp:Button ID="btnCancel" runat="server" Text="Cancel"  CssClass="btn btn-warning form-control" style="padding:10px; background-color:red; margin-right:50px" Width="146px" OnClick="btnCancel_Click" />
+             <asp:Button ID="btnSave" runat="server" Text="Save"  OnClientClick="return validate()" cssClass="btn btn-primary form-control"  style="background-color:green; margin-left:20px; padding:10px" Width="135px" OnClick="Button3_Click" />
+             <br />
+         </div>
+ 
+         </div>
+           <div>
+             <asp:Label ID="lblMsg" runat="server" Text=" "></asp:Label>
+         </div><br />
+
+     </div>
+                 </div>
+        </div>
+           </center>
+    </form>
+    <p>
+        &nbsp;</p>
+</body>
+</html>
---
+++ Registartion.aspx.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+
+namespace Banking_Customer_Regstartion_App
+{
+
+    public partial class Registartion : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection();
+        int status = 1;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            con.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB; Initial catalog=Banking; integrated security=true";
+            if (!IsPostBack)
+            {
+                ddlAccounts.DataSource = Accounts();
+                ddlAccounts.DataTextField = "Account";
+                ddlAccounts.DataValueField = "Id";
+                ddlAccounts.DataBind();
+                lblAccNum.Text = AccountNumber().ToString();
+                ViewState["action"] = 1;
+            }
+
+        }
+        int AccountNumber()
+        {
+            if (con.State == ConnectionState.Closed)
+                con.Open();
+            SqlCommand cmdAccnum = new SqlCommand("Select max(accno)+1 from Customers", con);
+            return Convert.ToInt32(cmdAccnum.ExecuteScalar());
+
+        }
+
+        DataTable Accounts()
+        {
+            SqlDataAdapter daActs = new SqlDataAdapter("Select Id,Account from Accounts", con);
+            DataTable dtActs = new DataTable();
+            daActs.Fill(dtActs);
+            DataRow r = dtActs.NewRow();
+            r[0] = -1;
+            r[1] = "Choose Preferred Account";
+            dtActs.Rows.InsertAt(r, 0);
+            return dtActs;
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(ddlAccounts.SelectedValue) == -1)
+            {
+                lblMsg.Text = "Please Choose Your Account Type";
+                lblMsg.ForeColor = Color.Red;
+                return;
+            }
+
+            if (!cbStatus.Checked)
+            {
+                status = 0;
+            }
+            SqlCommand cmdSave = new SqlCommand();
+            cmdSave.Connection = con;
+            if (con.State == ConnectionState.Closed)
+                con.Open();
+            if (Convert.ToInt32(ViewState["action"]) == 1)
+            {
+                cmdSave.CommandText = "insert into customers values(@accno, @name, @address, @account, @status)";
+            }
+            else if (Convert.ToInt32(ViewState["action"]) == 2)
+            {
+                cmdSave.CommandText = "update customers set name=@name, address=@address, account=@account, status=@status where accno=@accno";
+            }
+
+            cmdSave.Parameters.AddWithValue("@accno", Convert.ToInt32(lblAccNum.Text));
+            cmdSave.Parameters.AddWithValue("@name", txtName.Text.Trim());
+            cmdSave.Parameters.AddWithValue("@address", txtAdd.Text.Trim());
+            cmdSave.Parameters.AddWithValue("@account", Convert.ToInt32(ddlAccounts.SelectedValue));
+            cmdSave.Parameters.AddWithValue("@status", status);
+
+            try
+            {
+                int recordsAffected = cmdSave.ExecuteNonQuery();
+
+                if (recordsAffected > 0)
+                {
+                    if (Convert.ToInt32(ViewState["action"]) == 1)
+                    {
+                        lblMsg.Text = "Account has been opened for: " + txtName.Text.Trim();
+
+                    }
+                    btnSave.Enabled = false;
+                    lblMsg.ForeColor = Color.Red;
+                }
+                else
+                {
+                    lblMsg.Text = "Failed to Add the deatils";
+                    lblMsg.ForeColor = Color.Red;
+                }
+            }
+            catch (Exception ex)
+            {
+                lblMsg.Text = "Database Error: " + ex.Message;
+                lblMsg.ForeColor = Color.Red;
+            }
+        }
+
+        protected void btnCancel_Click(object sender, EventArgs e)
+        {
+            ViewState["action"] = 1;
+            status = 1;
+            btnSave.Enabled = true;
+            txtName.Text = " ";
+            txtAdd.Text = "";
+            ddlAccounts.SelectedIndex = 0;
+            lblMsg.Text = "";
+            lblAccNum.Text = AccountNumber().ToString();
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
